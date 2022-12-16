39d68ffc21dbf4146370694f3a175c20248beede
eren <nemadzonrahmatov2004@mail.ru>
2022-12-17 02:54:26 +0300

    1

--- informatika/lab4/proga2.py
+++ informatika/lab4/proga2.py
@@ -8,7 +8,7 @@ with open('jsonfile.json', encoding="utf-8") as js:
 	data = json.load(js)
 
 with open('yamlefile_programm_lib.yaml', 'w', encoding="utf-8") as yml:
-	yaml.dump(data, yml, allow_unicode=True)   
+	yaml.dump(data, yml, allow_unicode=True, sort_keys=False)   
 
 print(time.perf_counter() - start_time)
 #0.002262099995277822
